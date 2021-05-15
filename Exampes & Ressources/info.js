66cfd05db5b41a862b4ad5a040d743cff37d9f1b
Jan-Burger <jan.burger98eb.de>
2021-05-15 10:59:41 +0200

    Information for calc percentage function

---
+++ Exampes & Ressources/info.js
@@ -0,0 +1,10 @@
+// 1.  Input: startdatum enddatum [{date: 1999-10-10, price: 200.56}, {}, {}]
+// 2.  Output: [{date: startdatum, price: 0}, .... ,{date: enddatum, price: 50}]
+
+// Check if range startdatum - enddatum in arr --> console.log asdfdasf
+
+const calc_percentage = (Arr) => {
+
+
+    return Arr
+};
